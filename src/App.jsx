4c48f7dbfb57b6aeee470ebde9b63ac83306b3f1
anikasaida <anikasaida5@gmail.com>
2026-07-16 23:20:13 +0600

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        SkillSwap
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
